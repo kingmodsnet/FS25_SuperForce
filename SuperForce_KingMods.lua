717da5c2804c86811b320ab8b30b0444cced94e6
Monsieur Bab <baptiste.brossette@ymail.com>
2024-11-24 14:23:10 +0100

    Fixed issue when disconnecting/reconnecting from a multiplayer game

--- SuperForce_KingMods.lua
+++ SuperForce_KingMods.lua
@@ -3,8 +3,8 @@
 --
 -- Author: KingMods
 -- Contributors: Jean-Sébastien
--- Date: 22.11.2024
--- Version: 1.0.0.1
+-- Date: 24.11.2024
+-- Version: 1.0.0.2
 --
 -- https://www.kingmods.net/
 --
@@ -15,10 +15,22 @@ SuperForce.initialized = {}
 
 function SuperForce:update(dt)
     if g_currentMission.playerSystem ~= nil and g_currentMission.playerSystem.players ~= nil then
-        for userId, player in pairs(g_currentMission.playerSystem.players) do
-            if not SuperForce.initialized[userId] and player.hands ~= nil then
-                player.hands:consoleCommandToggleSuperStrength()
-                SuperForce.initialized[userId] = true
+        local activeUniqueIds = {}
+
+        for _, player in pairs(g_currentMission.playerSystem.players) do
+            activeUniqueIds[player.userId] = true
+
+            if not SuperForce.initialized[player.userId] and player.hands ~= nil then
+                if player.hands.consoleCommandToggleSuperStrength ~= nil then
+                    player.hands:consoleCommandToggleSuperStrength()
+                    SuperForce.initialized[player.userId] = true
+                end
+            end
+        end
+
+        for userId, _ in pairs(SuperForce.initialized) do
+            if not activeUniqueIds[userId] then
+                SuperForce.initialized[userId] = nil
             end
         end
     end
